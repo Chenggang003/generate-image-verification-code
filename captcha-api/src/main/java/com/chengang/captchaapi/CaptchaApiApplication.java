42505231c0a3e567a11df8260c283ae70d35dca9
chengang <chengang@vcredit.com>
2019-12-27 16:25:05 +0800

    初始化提交

---
+++ captcha-api/src/main/java/com/chengang/captchaapi/CaptchaApiApplication.java
@@ -0,0 +1,13 @@
+package com.chengang.captchaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaptchaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaptchaApiApplication.class, args);
+    }
+
+}
---
+++ captcha-api/src/test/java/com/chengang/captchaapi/CaptchaApiApplicationTests.java
@@ -0,0 +1,47 @@
+package com.chengang.captchaapi;
+
+import com.wf.captcha.ArithmeticCaptcha;
+import com.wf.captcha.SpecCaptcha;
+import com.wf.captcha.base.Captcha;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.awt.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.util.Arrays;
+
+
+@SpringBootTest
+class CaptchaApiApplicationTests {
+
+    private static final Logger logger = LoggerFactory.getLogger(CaptchaApiApplicationTests.class);
+
+    @Test
+    void contextLoads() throws Exception {
+        Captcha specCaptcha = new SpecCaptcha(130, 48);
+        specCaptcha.setCharType(Captcha.TYPE_ONLY_CHAR);
+        generateImageCode(specCaptcha);
+
+//        Captcha chineseCaptcha = new ChineseCaptcha(130, 48);
+//        generateImageCode(chineseCaptcha);
+
+//        Captcha arithmeticCaptcha = new ArithmeticCaptcha(130, 48);
+//        arithmeticCaptcha.setLen(3);
+//        arithmeticCaptcha.setFont(new Font("楷体", Font.PLAIN, 28));
+//        arithmeticCaptcha.setFont(Captcha.FONT_9);
+//        generateImageCode(arithmeticCaptcha);
+    }
+
+    private void generateImageCode(Captcha captcha) throws FileNotFoundException {
+        logger.info("验证码的的字符：{}", captcha.text());
+        logger.info("验证码的的字符数组：{}", Arrays.toString(captcha.textChar()));
+        logger.info("图片验证码BASE64进行编码：{}", captcha.toBase64());
+        OutputStream outputStream = new FileOutputStream(new File("E:/captcha/"+captcha.text()+".png"));
+        captcha.out(outputStream);
+    }
+}
